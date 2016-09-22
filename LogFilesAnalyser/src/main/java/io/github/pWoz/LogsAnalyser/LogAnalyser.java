fa7c3b5517df66a4b37b0dc1d2f2ada5ad23ad71
pWoz <pawel.wozniak@hotmail.com>
2016-09-22 21:54:43 +0200

    Response codes analysis

--- LogFilesAnalyser/src/main/java/io/github/pWoz/LogsAnalyser/LogAnalyser.java
+++ LogFilesAnalyser/src/main/java/io/github/pWoz/LogsAnalyser/LogAnalyser.java
@@ -1,12 +1,14 @@
 package io.github.pWoz.LogsAnalyser;
 
 import io.github.pWoz.LogsAnalyser.analysers.RddAnalyser;
+import io.github.pWoz.LogsAnalyser.analysers.ResponseCodesAnalyser;
 import io.github.pWoz.LogsAnalyser.analysers.ResponseTimesAnalyser;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.LongAccumulator;
 
 import java.util.List;
 
@@ -22,55 +24,67 @@ public class LogAnalyser {
     private static final int RESPONSE_CODE_POSITION = 7;
     private static final int RESPONSE_TIME_POSITION = 8;
     private static final int CLIENT_IP_POSITION = 9;
+    //
+    private JavaSparkContext sc;
+    private LongAccumulator errorsCounter;
+    private LongAccumulator successesCounter;
 
 
     public static void main(String[] args) {
-        LogAnalyser analyser  = new LogAnalyser();
-        JavaSparkContext sc = analyser.initContext();
-        JavaRDD<String> logFile = analyser.loadLogFile(sc);
-        analyser.performAnalysis(sc, logFile);
+        LogAnalyser analyser = new LogAnalyser();
+        JavaRDD<String> logFile = analyser.loadLogFile();
+        analyser.performAnalysis(logFile);
     }
 
-    private JavaSparkContext initContext(){
+    public LogAnalyser() {
+        sc = this.initContext();
+        errorsCounter = sc.sc().longAccumulator();
+        successesCounter = sc.sc().longAccumulator();
+    }
+
+    private JavaSparkContext initContext() {
         SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
         return new JavaSparkContext(conf);
     }
 
-    private JavaRDD<String> loadLogFile(JavaSparkContext sc){
+    private JavaRDD<String> loadLogFile() {
         return sc.textFile(LOG_FILE_NAME);
     }
 
-    private void performAnalysis(JavaSparkContext sc, JavaRDD<String> logFile) {
+    private void performAnalysis(JavaRDD<String> logFile) {
         JavaRDD<Integer> responseTimes = fetchResponseTimes(logFile);
-        JavaRDD<String> responseCodes  = fetchResponseCodes(logFile);
+        JavaRDD<String> responseCodes = fetchResponseCodes(logFile);
         JavaRDD<String> applicationNames = fetchApplicationNames(logFile);
         JavaRDD<String> clientIPs = fetchClientIPs(logFile);
         //
         RddAnalyser responseTimesAnalyser = new ResponseTimesAnalyser(responseTimes);
         responseTimesAnalyser.analyseRdd();
+        //
+        RddAnalyser responseCodesAnalyser = new ResponseCodesAnalyser(responseCodes, successesCounter, errorsCounter);
+        responseCodesAnalyser.analyseRdd();
     }
 
-    private JavaRDD<Integer> fetchResponseTimes(JavaRDD<String> logFile){
+    private JavaRDD<Integer> fetchResponseTimes(JavaRDD<String> logFile) {
         JavaRDD<String> responseTimesWithMs = logFile.map(s -> s.split(" ")[RESPONSE_TIME_POSITION]);
         JavaRDD<Integer> responseTimes = responseTimesWithMs.map(s -> Integer.parseInt(s.substring(0, s.length() - 2)));
         LOGGER.info("Response times fetched. Sample" + responseTimes.take(5));
         return responseTimes;
     }
 
-    private JavaRDD<String> fetchResponseCodes(JavaRDD<String> logFile){
+    private JavaRDD<String> fetchResponseCodes(JavaRDD<String> logFile) {
         JavaRDD<String> responseCodes = logFile.map(s -> s.split(" ")[RESPONSE_CODE_POSITION]);
         LOGGER.info("Response codes fetched. Sample" + responseCodes.take(5));
         return responseCodes;
     }
 
-    private JavaRDD<String> fetchApplicationNames(JavaRDD<String> logFile){
+    private JavaRDD<String> fetchApplicationNames(JavaRDD<String> logFile) {
         JavaRDD<String> applicationNames = logFile.map(s -> s.split(" ")[APP_NAME_POSITION]);
 
         LOGGER.info("Application names fetched. Sample" + applicationNames.take(5));
         return applicationNames;
     }
 
-    private JavaRDD<String> fetchClientIPs(JavaRDD<String> logFile){
+    private JavaRDD<String> fetchClientIPs(JavaRDD<String> logFile) {
         JavaRDD<String> clientIps = logFile.map(s -> s.split(" ")[CLIENT_IP_POSITION]);
 
         LOGGER.info("Client IPs fetched. Sample" + clientIps.take(5));
---
+++ LogFilesAnalyser/src/main/java/io/github/pWoz/LogsAnalyser/analysers/ResponseCodesAnalyser.java
@@ -0,0 +1,67 @@
+package io.github.pWoz.LogsAnalyser.analysers;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.util.LongAccumulator;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by pw on 2016-09-22.
+ */
+public class ResponseCodesAnalyser implements RddAnalyser, Serializable {
+
+    private JavaRDD<String> responseCodes = null;
+    private LongAccumulator errorsCounter;
+    private LongAccumulator successesCounter;
+
+    private static final List<Integer> successResponseCodes = Arrays.asList(200, 201);
+    private static final List<Integer> errorResponseCodes = Arrays.asList(404, 500);
+
+    public ResponseCodesAnalyser(JavaRDD<String> responseCodes, LongAccumulator successesCounter, LongAccumulator errorsCounter) {
+        this.responseCodes = responseCodes;
+        this.successesCounter = successesCounter;
+        this.errorsCounter = errorsCounter;
+    }
+
+    @Override
+    public void analyseRdd() {
+        countErrorResponses();
+        countSuccessResponses();
+        findTheMostCommonResponseTime();
+        System.out.println("/////////////////////");
+        System.out.println("Successes: " + successesCounter.value());
+        System.out.println("Errorrs: " + errorsCounter.value());
+        System.out.println("/////////////////////");
+    }
+
+    private void countErrorResponses() {
+        responseCodes.foreach(responseCode -> {
+            if (isErrorResponse(responseCode)) {
+                errorsCounter.add(1);
+            }
+        });
+    }
+
+    private void countSuccessResponses() {
+        responseCodes.foreach(responseCode -> {
+            if (isSuccessResponse(responseCode)) {
+                successesCounter.add(1);
+            }
+        });
+    }
+
+    private void findTheMostCommonResponseTime() {
+
+    }
+
+    private boolean isSuccessResponse(String responseValue) {
+        return successResponseCodes.contains(Integer.parseInt(responseValue));
+    }
+
+    private boolean isErrorResponse(String responseValue) {
+        return errorResponseCodes.contains(Integer.parseInt(responseValue));
+    }
+}
